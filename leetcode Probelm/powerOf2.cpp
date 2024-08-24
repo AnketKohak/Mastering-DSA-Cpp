0df3f9073d33ac55f7439c181008965b8a3f97dc
[Anket Kohak] <[anketkohak40@gmail.com]>
2024-08-24 19:57:03 +0530

    power of 2 file added

---
+++ leetcode Probelm/powerOf2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    int num =65;
+    for(int i =0;i<30;i++){
+        int powerof2 = pow(2,i);
+
+        if(powerof2==num){
+            cout<<"this is power of 2 number";
+            return 1;
+        }
+
+    }
+    cout<<"this not powerof 2";
+
+    return 0;
+}

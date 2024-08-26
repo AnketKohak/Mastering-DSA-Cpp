dfce8fe25e2d6bd3de8bb92ae9de03c29236c984
AnketKohak <anketkohak40@gmail.com>
2024-08-26 23:56:19 +0530

    unique number in array 10

---
+++ leetcode Probelm/uniqueNumInArry.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int numFinder(int arr[], int size)
+{
+    int ans = 0;
+    for (int i = 0; i < size; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+
+int main()
+{
+    cout << "Program start:" << endl;
+
+    int arr[10] = {1, 1, 2, 2, 3, 3, 4, 5, 5};
+    int a = numFinder(arr, 9);
+    cout << a << endl; 
+
+    cout << "Program end:" << endl;
+    return 0;
+}

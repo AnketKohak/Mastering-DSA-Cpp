dd5806fea71d7dd25ae5d0744366e0e76f28c271
AnketKohak <anketkohak40@gmail.com>
2024-08-29 22:42:50 +0530

    second for loop added

--- Pattern/asiapaintstar.cpp
+++ Pattern/asiapaintstar.cpp
@@ -1,13 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     cout << "Program start:" << endl;
-    int n =4;
-    for(in i =0;i<n;i++){
-        
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int i1 = n-i; i1 >= 1; i1--)
+        {
+            cout << " ";
+        }
+       
     }
-
 
     cout << "Program end:" << endl;
     return 0;

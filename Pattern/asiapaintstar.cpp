5645a6474cfe0ebd7636e6daad1dddb958aac346
AnketKohak <anketkohak40@gmail.com>
2024-08-29 22:31:09 +0530

    first loop added

---
+++ Pattern/asiapaintstar.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Program start:" << endl;
+    int n =4;
+    for(in i =0;i<n;i++){
+        
+    }
+
+
+    cout << "Program end:" << endl;
+    return 0;
+}

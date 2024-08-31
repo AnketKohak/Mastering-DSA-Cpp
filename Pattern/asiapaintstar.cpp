90ce7b63c21b1dcfa3c1bc088ad8c01b82cc457b
AnketKohak <anketkohak40@gmail.com>
2024-08-31 23:35:54 +0530

    tekna ..

--- Pattern/asiapaintstar.cpp
+++ Pattern/asiapaintstar.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
     cout << "Program start:" << endl;
-    
+
     int n = 4;
     for (int i = 0; i < n; i++)
-    {
+
+ {
         for (int i1 = n-i; i1 >= 1; i1--)
         {
             cout << " ";

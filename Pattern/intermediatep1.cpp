b087b9622b4e3e7af002f1c08fb6f93890c30ac9
AnketKohak <anketkohak40@gmail.com>
2024-09-02 21:35:57 +0530

    row loop added

--- Pattern/intermediatep1.cpp
+++ Pattern/intermediatep1.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     cout << "Program start:" << endl;
+    int n = 5;
+    for (int i = 0; i <= n; i++)
+    {
 
-   
+        
+    }
 
     cout << "Program end:" << endl;
     return 0;

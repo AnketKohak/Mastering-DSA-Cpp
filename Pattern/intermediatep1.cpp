bb2e061a72525dc5c02b2f00cb180ef9fd3b4ae2
AnketKohak <anketkohak40@gmail.com>
2024-09-02 21:45:07 +0530

    left loop added

--- Pattern/intermediatep1.cpp
+++ Pattern/intermediatep1.cpp
@@ -5,11 +5,19 @@ int main()
 {
     cout << "Program start:" << endl;
     int n = 5;
+    int count = 1;
     for (int row = 1; row <= n; row++)
     {
         for (int space = n - row; n >= 1; n--)
         {
+            cout << " ";
         }
+        for (int left = 1; left <= row; left++)
+        {
+            cout << count << " ";
+            count++;
+        }
+       
     }
 
     cout << "Program end:" << endl;

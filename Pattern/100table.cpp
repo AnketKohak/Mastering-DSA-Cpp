5c3d7588dda1c9e77dc8f47978c6f7d5e3d74eaf
AnketKohak <anketkohak40@gmail.com>
2024-09-05 17:12:02 +0530

    table of 100 number i

---
+++ Pattern/100table.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Program start:" << endl;
+
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=10;j++){
+            cout<<i*j<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout << "Program end:" << endl;
+    return 0;
+}

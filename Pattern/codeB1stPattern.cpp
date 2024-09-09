ffeb9a325816339026e02ce62fc8332c384119c5
AnketKohak <anketkohak40@gmail.com>
2024-09-09 23:36:26 +0530

    my best try ti solve this question

--- Pattern/codeB1stPattern.cpp
+++ Pattern/codeB1stPattern.cpp
@@ -1,11 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    int num = 10;
     cout << "Program start:" << endl;
 
-    // Your code here
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = i; j <= 4; j++)
+        {
+            if (i % 2 == 0)
+            {
+                cout << num++;
+            }
+            else
+            {
+                cout << num--;
+            }
+        }
+        cout << endl;
+    }
 
     cout << "Program end:" << endl;
     return 0;
 }
+
+// 10987
+// 456
+// 32
+// 1

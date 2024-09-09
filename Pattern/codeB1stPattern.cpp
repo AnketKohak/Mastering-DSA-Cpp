e56327f6ccfe687c86438c284ddec2724a2457e8
AnketKohak <anketkohak40@gmail.com>
2024-09-09 23:57:38 +0530

    hard code done but not satisfied

--- Pattern/codeB1stPattern.cpp
+++ Pattern/codeB1stPattern.cpp
@@ -1,30 +1,43 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    int num = 10;
+int main(){
+    int num =10;
+    bool assending=false;
     cout << "Program start:" << endl;
 
-    for (int i = 1; i <= 4; i++)
-    {
-        for (int j = i; j <= 4; j++)
-        {
-            if (i % 2 == 0)
-            {
-                cout << num++;
+    for(int i =1;i<=4;i++){
+        if (assending)
+        { 
+            if(num>3){
+            num=num-2;
             }
-            else
+            for (int j = i; j <= 4; j++)
             {
-                cout << num--;
+                cout << num;
+                num++;
+                assending=false;
             }
+           
+            cout << endl;
+          continue;
         }
-        cout << endl;
+
+            if(i==3){
+                num=num-4;
+            }
+        for(int j=i;j<=4;j++){
+            cout<<num;
+            num--;
+            assending = true;
+        }
+        cout<<endl;
     }
 
     cout << "Program end:" << endl;
     return 0;
 }
+
 
 // 10987
 // 456

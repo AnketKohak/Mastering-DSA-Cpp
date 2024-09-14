627ecdf13f0e8d9357070cb1b82d4e6daebcb976
AnketKohak <anketkohak40@gmail.com>
2024-09-15 00:53:19 +0530

    tekna ..

---
+++ Binary Serach/mountainPeak.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int peak(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+
+    while (start < end)
+    {
+        if (arr[mid] < arr[mid + 1])
+        {
+            start = mid+1;
+
+        }
+        else
+        {
+            end = mid;
+        }
+        mid = (start+end)/2;
+    }
+    return arr[start];
+}
+
+int main()
+{
+    cout << "Program start:" << endl;
+    int arr[9] = {0,1, 2,3,4,3,2, 1,0};
+    int size = 9;
+    cout << "peak element : " <<peak(arr, size) << endl;
+
+    cout << "Program end:" << endl;
+    return 0;
+}

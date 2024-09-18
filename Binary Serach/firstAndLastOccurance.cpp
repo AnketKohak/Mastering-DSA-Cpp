d80f04146840cadcbf1939965a2a0e97aeee51b9
AnketKohak <anketkohak40@gmail.com>
2024-09-18 23:37:19 +0530

    some change

--- Binary Serach/firstAndLastOccurance.cpp
+++ Binary Serach/firstAndLastOccurance.cpp
@@ -1,10 +1,65 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int first(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int first = -1;
+    int mid = (start + end) / 2;
+    while (start <= end)
+    {
+        if (key == arr[mid])
+        {
+            first = mid;
+            end = mid - 1;
+        }
+        else if(key<arr[mid]){
+            end= mid-1;
+        }
+        else if(key>arr[mid]){
+            start = mid+1;
+        }
+        mid = (start + end) / 2;
+    }
+    return first;
+}
+
+int last(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int first = -1;
+    int mid = (start + end) / 2;
+    while (start <=end)
+    {
+        if (key == arr[mid])
+        {
+            first = mid;
+            start = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return first;
+}
+
+int main()
+{
     cout << "Program start:" << endl;
 
-    // Your code here
+    int arr[10] = {0,0,1,1,4,6,7,8,9,10};
+    int size= 10;
+    int key =1;
+    cout<<"first : "<<first(arr,size,key)<<endl;
+    cout << "last : " << last(arr, size, key) << endl;
 
     cout << "Program end:" << endl;
     return 0;
